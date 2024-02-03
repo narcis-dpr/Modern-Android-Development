bc5ad492992936abf8d563751fea4dbfb4ae43ca
n.dipir <nargesdeypir@gmail.com>
2024-02-03 09:41:19 +0330

    basic phase -- sample border button

--- app/src/main/java/com/narcis/modern/MainActivity.kt
+++ app/src/main/java/com/narcis/modern/MainActivity.kt
@@ -3,13 +3,24 @@ package com.narcis.modern
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.narcis.modern.ui.theme.ModernAndroidDevelopmentTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +29,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             ModernAndroidDevelopmentTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                        .wrapContentSize(Alignment.Center),
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    Greeting("Android")
+                    Greeting(name = "Hello modern citizens")
                 }
             }
         }
@@ -31,10 +43,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+        )
+        SampleButton()
+    }
+}
+
+@Composable
+fun SampleButton() {
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = Modifier.fillMaxWidth()
+            .fillMaxWidth()
+            .padding(24.dp),
+        shape = RoundedCornerShape(20.dp),
+        border = BorderStroke(2.dp, Color.Blue),
+        colors = ButtonDefaults.buttonColors(
+            contentColor = Color.Gray,
+            containerColor = Color.White,
+        ),
+    ) {
+        Text(
+            text = stringResource(id = R.string.click_me),
+            fontSize = 14.sp,
+            modifier = Modifier.padding(horizontal = 30.dp, vertical = 6.dp),
+        )
+    }
 }
 
 @Preview(showBackground = true)
