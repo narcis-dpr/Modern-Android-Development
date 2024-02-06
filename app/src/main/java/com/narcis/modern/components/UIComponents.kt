a659e5bca863b1667a2d41d2c56a5b2478f60aba
n.dipir <nargesdeypir@gmail.com>
2024-02-06 09:47:15 +0330

    basic phase -- buttons with shapes and icons

--- app/src/main/java/com/narcis/modern/components/UIComponents.kt
+++ app/src/main/java/com/narcis/modern/components/UIComponents.kt
@@ -2,9 +2,16 @@ package com.narcis.modern.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
@@ -12,6 +19,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,12 +62,66 @@ fun NotOutlinedEditTextExample() {
     )
 }
 
+@Composable
+fun ButtonWithIcon() {
+    Button(onClick = { /*TODO*/ }) {
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_10k_24),
+            contentDescription = stringResource(id = R.string.shop),
+            modifier = Modifier.padding(10.dp),
+        )
+        Text(
+            text = stringResource(id = R.string.buy),
+            modifier = Modifier.padding(start = 10.dp),
+        )
+    }
+}
+
+@Composable
+fun CornerCutShapeButton() {
+    Button(onClick = { /*TODO*/ }, shape = CutCornerShape(10)) {
+        Text(text = stringResource(id = R.string.cornerButton))
+    }
+}
+
+@Composable
+fun RoundCornerShapeButton() {
+    Button(
+        onClick = { /*TODO*/ },
+        shape = RoundedCornerShape(10.dp),
+    ) {
+        Text(text = stringResource(id = R.string.rounded))
+    }
+}
+
+@Composable
+fun ElevatedButtonExample() {
+    Button(
+        onClick = { /*TODO*/ },
+        elevation = ButtonDefaults.buttonElevation(
+            defaultElevation = 8.dp,
+            pressedElevation = 10.dp,
+            disabledElevation = 0.dp,
+        ),
+    ) {
+        Text(text = stringResource(id = R.string.elevated))
+    }
+}
+
 @Preview
 @Composable
 private fun Preview() {
     Column(modifier = Modifier.fillMaxSize().background(color = Color.White)) {
-        EditTextExample()
-
-        NotOutlinedEditTextExample()
+//        EditTextExample()
+//
+//        NotOutlinedEditTextExample()
+        ButtonWithIcon()
+        Spacer(modifier = Modifier.height(10.dp))
+        CornerCutShapeButton()
+        Spacer(modifier = Modifier.height(10.dp))
+        RoundCornerShapeButton()
+        Spacer(modifier = Modifier.height(10.dp))
+        ElevatedButtonExample()
+        Spacer(modifier = Modifier.height(10.dp))
     }
 }
