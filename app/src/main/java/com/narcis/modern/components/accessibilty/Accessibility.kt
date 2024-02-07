ba5ecf8d3edc311ed021b493a7f58b46447dfa1f
n.dipir <nargesdeypir@gmail.com>
2024-02-07 10:20:32 +0330

    basic phase -- add Accessibility to a sample compose: semantic

--- app/src/main/java/com/narcis/modern/components/accessibilty/Accessibility.kt
+++ app/src/main/java/com/narcis/modern/components/accessibilty/Accessibility.kt
@@ -1,9 +1,15 @@
 package com.narcis.modern.components.accessibilty
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.dp
 import com.narcis.modern.R
 
 @Composable
@@ -11,5 +17,10 @@ fun ImageWithAccessibility() {
     Image(
         painter = painterResource(id = R.drawable.bernn),
         contentDescription = stringResource(id = R.string.city_images),
+    )
+    Text(
+        text = stringResource(id = R.string.sample),
+        modifier = Modifier.padding(18.dp)
+            .semantics { heading() },
     )
 }
