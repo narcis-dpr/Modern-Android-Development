68083ec91ae9a4c7d9fef0cb7a39ce8507490a49
narcis-dpr <nargesdeypir@gmail.com>
2024-04-05 11:50:56 +0330

    android test -- test tags

---
+++ androidtest/src/main/java/com/example/androidtest/sampleLogin/util/TestTags.kt
@@ -0,0 +1,11 @@
+package com.example.androidtest.sampleLogin.util
+
+object TestTags {
+    object LoginContent {
+        const val SIGN_IN_BUTTON = "sign_in_button"
+        const val LOGO_IMAGE = "logo_image_button"
+        const val ANDROID_TEXT = "community_text"
+        const val USERNAME_FIELD = "username_field"
+        const val PASSWORD_FIELD = "password_field"
+    }
+}
