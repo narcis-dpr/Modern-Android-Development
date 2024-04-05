230b847cacca1a02a08f73843981be185d3c110e
narcis-dpr <nargesdeypir@gmail.com>
2024-04-05 12:29:18 +0330

    android test -- copy paste AuthenticationState

---
+++ androidtest/src/main/java/com/example/androidtest/sampleLogin/state/AuthenticationState.kt
@@ -0,0 +1,16 @@
+package com.example.androidtest.sampleLogin.state
+
+data class AuthenticationState(
+    val userName: String = "",
+    val password: String = "",
+    var togglePasswordVisibility: Boolean = true,
+    val loading: Boolean = false,
+) {
+    fun isValidForm(): Boolean {
+        return userName.isNotEmpty() && password.isNotEmpty()
+    }
+
+    companion object {
+        val EMPTY_STATE = AuthenticationState()
+    }
+}
