42aa082b8fcea52f1e191ab1e27891780dc4cd03
narcis-dpr <nargesdeypir@gmail.com>
2024-04-11 10:43:16 +0330

    navigation in android -- navigation dependencies

--- navigation/src/main/java/com/narcis/navigation/ui/theme/Color.kt
+++ navigation/src/main/java/com/narcis/navigation/ui/theme/Color.kt
@@ -3,6 +3,7 @@ package com.narcis.navigation.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
+val Purple700 = Color(0xFF673AB7)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
