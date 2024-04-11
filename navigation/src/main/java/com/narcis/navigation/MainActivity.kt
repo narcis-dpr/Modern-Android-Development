d0020ff7905187632e926f09d5fe310404fdbe83
narcis-dpr <nargesdeypir@gmail.com>
2024-04-11 10:44:04 +0330

    navigation in android -- BottomNavigationBar

--- navigation/src/main/java/com/narcis/navigation/MainActivity.kt
+++ navigation/src/main/java/com/narcis/navigation/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.narcis.navigation.ui.theme.ModernAndroidDevelopmentTheme
+import com.narcis.navigation.ui.theme.Purple700
+import com.narcis.navigation.util.Destination
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +37,40 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@Composable
+fun BottomNavigationBar(navController: NavController, appItems: List<Destination>) {
+    BottomNavigation(
+        backgroundColor = Purple700,
+        contentColor = Color.White
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        appItems.forEach { item ->
+            BottomNavigationItem(
+                icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
+                label = { Text(text = item.title) },
+                selectedContentColor = Color.White,
+                unselectedContentColor = Color.White.copy(0.4f),
+                alwaysShowLabel = true,
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
