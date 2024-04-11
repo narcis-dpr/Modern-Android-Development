c30946cc3aaef7820d6e93c1f5d046fe92205763
narcis-dpr <nargesdeypir@gmail.com>
2024-04-11 19:42:54 +0330

    navigation in android -- navigation with starter screen

--- navigation/src/main/java/com/narcis/navigation/MainActivity.kt
+++ navigation/src/main/java/com/narcis/navigation/MainActivity.kt
@@ -3,11 +3,14 @@ package com.narcis.navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,7 +20,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.narcis.navigation.ui.theme.ModernAndroidDevelopmentTheme
 import com.narcis.navigation.ui.theme.Purple700
 import com.narcis.navigation.util.Destination
@@ -32,12 +39,46 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    AppScreen()
                 }
             }
         }
     }
 }
 
+@Composable
+fun AppScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar(navController = navController, appItems = Destination.toList)
+        },
+        content = {padding ->
+            Box(
+                modifier = Modifier.padding(padding)
+            ) {
+                AppNavigation(navController = navController)
+            }
+        }
+    )
+}
+@Composable
+fun AppNavigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Destination.Transaction.route) {
+        composable(Destination.Transaction.route) {
+            Transaction()
+        }
+        composable(Destination.Budgets.route) {
+            Budget()
+        }
+        composable(Destination.Tasks.route) {
+            Tasks()
+        }
+        composable(Destination.Settings.route) {
+            Settings()
+        }
+    }
+}
 @Composable
 fun BottomNavigationBar(navController: NavController, appItems: List<Destination>) {
     BottomNavigation(
@@ -68,7 +109,6 @@ fun BottomNavigationBar(navController: NavController, appItems: List<Destination
             )
         }
     }
-
 }
 
 
