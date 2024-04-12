1ea9c464709f12c0a9589fc9d49d1954e0f1cfb7
narcis-dpr <nargesdeypir@gmail.com>
2024-04-12 17:43:06 +0330

    recreate sample login -- add new destination

--- samplelogin/src/main/java/com/example/samplelogin/util/Destination.kt
+++ samplelogin/src/main/java/com/example/samplelogin/util/Destination.kt
@@ -1,6 +1,7 @@
 package com.example.samplelogin.util
 
 sealed class Destination (val route: String){
+    object Main: Destination("main_route")
     object LoginScreen: Destination("login_screen")
     object RegisterScreen: Destination("register_screen")
 }
