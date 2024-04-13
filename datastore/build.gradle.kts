9af85b523ffd3941bb8d77f438f0bce9d9a98778
narcis-dpr <nargesdeypir@gmail.com>
2024-04-13 23:10:10 +0330

    data store -- data store manager with implementation

--- datastore/build.gradle.kts
+++ datastore/build.gradle.kts
@@ -68,4 +68,5 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation("androidx.DataStore:DataStore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore-preferences-core:1.0.0")
 }
--- datastore/src/main/java/com/narcis/datastore/DataStoreManagerImpl.kt
+++ datastore/src/main/java/com/narcis/datastore/DataStoreManagerImpl.kt
@@ -1,13 +1,29 @@
 package com.narcis.datastore
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class DataStoreManagerImpl(): DataStoreManager {
+class DataStoreManagerImpl(private val tasksPreferenceStore: DataStore<Preferences>): DataStoreManager {
+    private val FIRST_TASK = stringPreferencesKey("first_task")
+    private val SECOND_TASK = stringPreferencesKey("second_task")
+    private val THIRD_TASK = stringPreferencesKey("third_task")
     override suspend fun saveTasks(tasks: Tasks) {
-        TODO("Not yet implemented")
+        tasksPreferenceStore.edit { tasksPreferenceStore ->
+            tasksPreferenceStore[FIRST_TASK] = tasks.firstTask
+            tasksPreferenceStore[SECOND_TASK] = tasks.secondTask
+            tasksPreferenceStore[THIRD_TASK] = tasks.thirdTask
+        }
     }
 
-    override fun getTasks(): Flow<Tasks> {
-        TODO("Not yet implemented")
+    override fun getTasks(): Flow<Tasks> = tasksPreferenceStore.data.map {tasksPreference ->
+        Tasks(
+            firstTask = tasksPreference[FIRST_TASK] ?: "",
+            secondTask = tasksPreference[SECOND_TASK] ?: "",
+            thirdTask = tasksPreference[THIRD_TASK] ?: ""
+        )
     }
 }
