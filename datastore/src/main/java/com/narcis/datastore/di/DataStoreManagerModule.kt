ea5d97467d1ecb8c72001c256f378b2845808121
n.dipir <nargesdeypir@gmail.com>
2024-04-15 09:53:56 +0330

    data store -- di modules

---
+++ datastore/src/main/java/com/narcis/datastore/di/DataStoreManagerModule.kt
@@ -0,0 +1,17 @@
+package com.narcis.datastore.di
+
+import com.narcis.datastore.DataStoreManager
+import com.narcis.datastore.DataStoreManagerImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataStoreManagerModule {
+    @Singleton
+    @Binds
+    abstract fun bindDataStoreRepository(dataStoreManagerImpl: DataStoreManagerImpl): DataStoreManager
+}
--- datastore/src/main/java/com/narcis/datastore/di/DataStoreModule.kt
+++ datastore/src/main/java/com/narcis/datastore/di/DataStoreModule.kt
@@ -8,9 +8,18 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import androidx.datastore.preferences.preferencesDataStore
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DataStoreModule {
     private val Context.tasksPreferenceStore : DataStore<Preferences> by preferencesDataStore(name = "store_tasks")
+
+    @Singleton
+    @Provides
+    fun provideTasksPreferenceDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<Preferences> = context.tasksPreferenceStore
 }
