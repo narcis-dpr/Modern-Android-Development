cc6d7aa90532a98f4f54aee845a3fa7c89651370
n.dipir <nargesdeypir@gmail.com>
2024-04-17 10:14:11 +0330

    data store -- add proto data store in di

--- datastore/src/main/java/com/narcis/datastore/di/DataStoreModule.kt
+++ datastore/src/main/java/com/narcis/datastore/di/DataStoreModule.kt
@@ -8,6 +8,8 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import androidx.datastore.preferences.preferencesDataStore
+import com.narcis.datastore.TaskPreference
+import com.narcis.datastore.data.TaskSerializer
 import dagger.Provides
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Singleton
@@ -15,11 +17,21 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class DataStoreModule {
-    private val Context.tasksPreferenceStore : DataStore<Preferences> by preferencesDataStore(name = "store_tasks")
+    private val Context.tasksPreferenceStore: DataStore<Preferences> by preferencesDataStore(name = "store_tasks")
+    private val Context.taskProtoDataStore: DataStore<TaskPreference> by dataStore(
+        fileName = "task.pd",
+        serializer = TaskSerializer
+    )
 
     @Singleton
     @Provides
     fun provideTasksPreferenceDataStore(
         @ApplicationContext context: Context
     ): DataStore<Preferences> = context.tasksPreferenceStore
+
+    @Singleton
+    @Provides
+    fun provideTasksProtoDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<TaskPreference> = context.taskProtoDataStore
 }
