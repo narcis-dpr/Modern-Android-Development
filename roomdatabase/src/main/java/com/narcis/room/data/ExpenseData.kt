f7efbd0a07be8d143308ab07e9b40c87c4da6f6d
n.dipir <nargesdeypir@gmail.com>
2024-04-21 10:57:09 +0330

    room database : add embedded val

--- roomdatabase/src/main/java/com/narcis/room/data/ExpenseData.kt
+++ roomdatabase/src/main/java/com/narcis/room/data/ExpenseData.kt
@@ -1,6 +1,7 @@
 package com.narcis.room.data
 
 import androidx.room.ColumnInfo
+import androidx.room.Embedded
 import androidx.room.Entity
 
 @Entity(tableName = "expenses")
@@ -12,5 +13,11 @@ data class ExpenseData(
     @ColumnInfo(name = "update_at")
     var expenseDate: String = "",
     var note: String = "",
-    var currency: String = ""
+    var currency: String = "",
+    @Embedded
+    val tasks: Tasks
+)
+
+data class Tasks(
+    val id: Int = 0
 )
--- roomdatabase/src/main/java/com/narcis/room/data/dao/UserInformationDatabase.kt
+++ roomdatabase/src/main/java/com/narcis/room/data/dao/UserInformationDatabase.kt
@@ -2,10 +2,17 @@ package com.narcis.room.data.dao
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import com.narcis.room.data.BudgetData
+import com.narcis.room.data.ExpenseData
+import com.narcis.room.data.ExpenseItem
 import com.narcis.room.data.UserInformationModel
 
-@Database(entities = [UserInformationModel::class], version = 1, exportSchema = false)
-abstract class UserInformationDatabase: RoomDatabase() {
+@Database(
+    entities = [UserInformationModel::class, ExpenseItem::class,
+        BudgetData::class, ExpenseData::class], version = 1, exportSchema = false
+)
+abstract class UserInformationDatabase : RoomDatabase() {
     abstract fun userInformationDao(): UserInformationDao
     abstract fun budgetDao(): BudgetDao
     abstract fun itemDao(): ItemDao
