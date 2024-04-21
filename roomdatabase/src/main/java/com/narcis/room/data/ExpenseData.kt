294ebdf48a2e869cfc016779a4dc04d32d3b8a9f
n.dipir <nargesdeypir@gmail.com>
2024-04-21 10:31:52 +0330

    room database : multiple entity: Expense Data

---
+++ roomdatabase/src/main/java/com/narcis/room/data/ExpenseData.kt
@@ -0,0 +1,16 @@
+package com.narcis.room.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+
+@Entity(tableName = "expenses")
+data class ExpenseData(
+    var id: Int = 0,
+    var expenseName: String = "",
+    var expenseType: String = "",
+    var expenseAmount: Double = 0.0,
+    @ColumnInfo(name = "update_at")
+    var expenseDate: String = "",
+    var note: String = "",
+    var currency: String = ""
+)
