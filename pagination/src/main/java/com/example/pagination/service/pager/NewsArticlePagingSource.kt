97be6c0a57c0622c17e4e7a755a097da2436c8d5
narcis-dpr <nargesdeypir@gmail.com>
2024-04-24 13:55:11 +0330

    pagination -- news article paging source

---
+++ pagination/src/main/java/com/example/pagination/service/pager/NewsArticlePagingSource.kt
@@ -0,0 +1,31 @@
+package com.example.pagination.service.pager
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.pagination.data.NewsArticle
+import com.example.pagination.service.NewsApiService
+
+class NewsArticlePagingSource(
+    private val newsApiService: NewsApiService
+) : PagingSource<Int, NewsArticle>() {
+    override fun getRefreshKey(state: PagingState<Int, NewsArticle>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, NewsArticle> {
+        return try {
+            val page = params.key ?: 1
+            val response = newsApiService.getNews(page = page)
+            LoadResult.Page(
+                data = response.articles,
+                prevKey = if (page == 1) null else page.minus(1),
+                nextKey = if (response.articles.isEmpty()) null else page.plus(1),
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+}
