03d05743da044ac8e8ea2e6b2d4b719df61af89a
narcis-dpr <nargesdeypir@gmail.com>
2024-04-26 19:11:38 +0330

    custom pagination -- StudentRepository

---
+++ custompagination/src/main/java/com/example/custompagination/repository/StudentRepository.kt
@@ -0,0 +1,26 @@
+package com.example.custompagination.repository
+
+import com.example.custompagination.data.StudentProfile
+import kotlinx.coroutines.delay
+
+class StudentRepository {
+    private val ourDataSource = (1..100).map {
+        StudentProfile(
+            name = "Student $it",
+            school = "MIT",
+            major = "Computer Science"
+        )
+    }
+
+    suspend fun getStudents(page: Int, pageSize: Int): Result<List<StudentProfile>> {
+        delay(timeMillis = 2000L)
+        val start = page * pageSize
+        return if(start + pageSize <= ourDataSource.size) {
+            Result.success(
+                ourDataSource.slice(start until start + pageSize)
+            )
+        } else {
+            Result.success(emptyList())
+        }
+    }
+}
