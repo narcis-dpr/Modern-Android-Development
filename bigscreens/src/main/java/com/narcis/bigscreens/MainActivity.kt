482e39c2b70dca594bbe9e476fc3d597213b7f5c
n.dipir <nargesdeypir@gmail.com>
2024-04-28 10:21:26 +0330

    big screens --- handle variables in main

--- bigscreens/src/main/java/com/narcis/bigscreens/MainActivity.kt
+++ bigscreens/src/main/java/com/narcis/bigscreens/MainActivity.kt
@@ -7,18 +7,56 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.window.layout.FoldingFeature
+import androidx.window.layout.WindowInfoTracker
 import com.narcis.bigscreens.ui.theme.ModernAndroidDevelopmentTheme
+import com.narcis.bigscreens.util.DevicePosture
+import com.narcis.bigscreens.util.cityPosture
+import com.narcis.bigscreens.util.separating
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val postureStateFlow = WindowInfoTracker.getOrCreate(this).windowLayoutInfo(this)
+            .flowWithLifecycle(this.lifecycle)
+            .map { layoutInfo ->
+                val foldingFeature = layoutInfo.displayFeatures.filterIsInstance<FoldingFeature>()
+                    .firstOrNull()
+
+                when {
+                    cityPosture(foldingFeature) ->
+                        DevicePosture.CityPosture(foldingFeature.bounds)
+
+                    separating(foldingFeature) ->
+                        DevicePosture.Separating(foldingFeature.bounds, foldingFeature.orientation)
+
+                    else -> DevicePosture.NormalPosture
+                }
+            }
+            .stateIn(
+                scope = lifecycleScope,
+                started = SharingStarted.Eagerly,
+                initialValue = DevicePosture.NormalPosture
+            )
+
         setContent {
             ModernAndroidDevelopmentTheme {
-                // A surface container using the 'background' color from the theme
+                val windowSize = calculateWindowSizeClass(activity = this)
+                val devicePosture = postureStateFlow.collectAsState().value
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
