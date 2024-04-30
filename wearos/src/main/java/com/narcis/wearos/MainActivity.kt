f7fc663b4bcd043ab697ad5ee3e1d0ba0bb5ad71
n.dipir <nargesdeypir@gmail.com>
2024-04-30 18:56:21 +0330

    wear os -- add another button with text to wear screen

--- wearos/src/main/java/com/narcis/wearos/MainActivity.kt
+++ wearos/src/main/java/com/narcis/wearos/MainActivity.kt
@@ -4,9 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountBox
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,6 +23,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.TimeText
 import com.narcis.wearos.ui.theme.ModernAndroidDevelopmentTheme
 
@@ -34,6 +44,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
 fun WearApp(greetingName: String) {
     ModernAndroidDevelopmentTheme {
@@ -43,16 +54,41 @@ fun WearApp(greetingName: String) {
                 .background(androidx.wear.compose.material.MaterialTheme.colors.background),
             contentAlignment = Alignment.Center
         ) {
-            SampleButton()
+            Sample2Button()
 
         }
     }
 }
+
 @Composable
 fun SampleButton() {
-    Button(onClick = { /*TODO*/ },
-        modifier = Modifier.fillMaxWidth()) {
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = Modifier.fillMaxWidth()
+    ) {
         Text(text = "Click me")
+    }
+}
+
+@Composable
+fun Sample2Button() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 10.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.size(32.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.AccountBox, contentDescription = "account box icon",
+                modifier = Modifier
+                    .size(24.dp)
+                    .wrapContentSize(align = Alignment.Center)
+            )
+        }
     }
 }
 
