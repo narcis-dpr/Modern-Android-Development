76e170015db55f231dbcd0cd80b8bc522eccdede
n.dipir <nargesdeypir@gmail.com>
2024-04-30 18:36:45 +0330

    wear os -- add a button with text to wear screen

--- wearos/src/main/java/com/narcis/wearos/MainActivity.kt
+++ wearos/src/main/java/com/narcis/wearos/MainActivity.kt
@@ -3,13 +3,19 @@ package com.narcis.wearos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.wear.compose.material.TimeText
 import com.narcis.wearos.ui.theme.ModernAndroidDevelopmentTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,14 +28,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    WearApp("Android")
                 }
             }
         }
     }
 }
+@Composable
+fun WearApp(greetingName: String) {
+    ModernAndroidDevelopmentTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(androidx.wear.compose.material.MaterialTheme.colors.background),
+            contentAlignment = Alignment.Center
+        ) {
+            SampleButton()
 
-
+        }
+    }
+}
+@Composable
+fun SampleButton() {
+    Button(onClick = { /*TODO*/ },
+        modifier = Modifier.fillMaxWidth()) {
+        Text(text = "Click me")
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
