e07c5f3a058359eb2034509b76ca1b6cbc3a10a3
n.dipir <nargesdeypir@gmail.com>
2024-05-06 10:12:16 +0330

    GUI Alerts -- radio button example

--- guialerrts/src/main/java/com/example/guialerrts/components/RadioButtonExample.kt
+++ guialerrts/src/main/java/com/example/guialerrts/components/RadioButtonExample.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -35,4 +36,10 @@ fun RadioButtonExample() {
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowRadioButton(){
+    RadioButtonExample()
 }
